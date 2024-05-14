3e124b1c3b838bd1bce26ff202e2fc1df613275e
vanja19 <student@rc355l.zjb.pmf.lan>
2024-05-14 09:24:50 +0200

    Obrisan komentar

--- TreciPrakticni/src/Primer.java
+++ TreciPrakticni/src/Primer.java
@@ -1,16 +1,10 @@
 
 public class Primer {
-	
-	/*calcultes sum of first n elements
-	 * 
-	 * n needs to be a positive number
-	 * 
-	 * */
-	
+
 	public int sum(int n) {
 		int result = 0;
-		for(int i = 1; i < n; i++) {
-			result = i;
+		for(int i = 1; i <= 10; i++) {
+			result += i;
 		}
 		return result;
 	}
